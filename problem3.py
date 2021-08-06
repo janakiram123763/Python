031aed944b8844bb9f19dfa390679b245a48e4bc
Janakiram <59314236+janakiram123763@users.noreply.github.com>
2021-08-06 14:37:04 +0530

    First and last

---
+++ problem3.py
@@ -0,0 +1,5 @@
+Alist = ['Sun','Mon','Tue','Wed','Thu', 'Fri','Sat']
+print("The given list : ",Alist)
+
+print("The first element of the list : ",Alist[0])
+print("The last element of the list : ",Alist[-1])
